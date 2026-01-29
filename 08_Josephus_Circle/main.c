258839a2ba6401312984ba41467ea010c841905e
XinRuLiuShui <1677409816@qq.com>
2026-01-29 14:42:56 +0800

    约瑟夫环，未释放删除节点内存

---
+++ 08_Josephus_Circle/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct node {
+	int data;
+	struct node* pNext;
+}JoeLinkListNode;
+
+
+//头插法创建单链表
+//arr:传入的顺序表 length:顺序表的长度
+JoeLinkListNode* CreateLinkList(int arr[], int length)
+{
+	JoeLinkListNode* pHead, * p, * q;
+	pHead = (JoeLinkListNode*)malloc(sizeof(JoeLinkListNode));
+	pHead->data = arr[0];
+	//q获得当前链表的头节点
+	q = pHead;
+	for (int i = 1; i < length; i++)
+	{
+		p = (JoeLinkListNode*)malloc(sizeof(JoeLinkListNode));
+		p->data = arr[i];
+		q->pNext = p;
+		q = p;
+	}
+	q->pNext = pHead;
+	return q;
+}
+
+int main()
+{
+	int temp[30];
+	for (int i = 0; i < 30; i++)
+	{
+		temp[i] = i;
+	}
+	JoeLinkListNode* HeadNode = CreateLinkList(temp, 30);
+	JoeLinkListNode* pre, * next;
+	JoeLinkListNode* cur = HeadNode;
+	int start = 1;	//从第一个人开始
+	int sum = 30;
+	int survivors = 15;	//剩余15人
+	int key = 9;	//逢9退出
+
+	pre = NULL;
+	next = NULL;
+	while (sum > 15)
+	{
+		for (int i = 1; i <= key; i++)
+		{
+			pre = cur;
+			cur = cur->pNext;
+			next = cur->pNext;
+		}
+		//next = cur->pNext;
+		// 删除cur结点
+		pre->pNext = next;
+		printf("删除节点%d \n", cur->data);
+		//free(cur);
+
+		sum--;
+	}
+}
