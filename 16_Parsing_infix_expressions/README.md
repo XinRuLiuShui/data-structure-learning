d67663ae1acbc15703eb953791a0c5773ff5022e
XinRuLiuShui <1677409816@qq.com>
2026-02-01 15:38:47 +0800

    链栈实现中缀表达式

---
+++ 16_Parsing_infix_expressions/README.md
@@ -0,0 +1,68 @@
+﻿> Written with [StackEdit](https://stackedit.io/).
+
+## 注意
+
+**本例的表达式只能使用个位的数，大于等于10的数会出错**
+
+## 问题
+
+在 `main()` 中：
+
+```c
+char num1 = 0;    // 1字节
+char num2 = 0;    // 1字节
+char operator=0;  // 1字节
+```
+
+但 `Pop()` 函数期望的是 `datatype*`（即 `int*`，4字节）：
+
+```c
+LinkStack* Pop(LinkStack* top, datatype* value)  // datatype = int
+{
+    *value = top->data;  // 向地址写入 4 字节（int）！
+    ...
+}
+```
+
+当执行 `*value = top->data` 时，会向 `num1` 或 `num2` 的地址**写入 4 个字节**，但 `char` 变量只分配了 **1 个字节**！多出的 3 个字节会**覆盖相邻变量的栈内存**，破坏了栈保护Cookie，触发 "Stack around the variable 'num2' was corrupted"。
+
+## 修复方案
+
+将 `main()` 中的变量类型改为 `int`（或 `datatype`）
+
+```c
+// ❌ 错误：char 只占1字节，Pop函数写入4字节导致溢出
+// char operator=0;
+// char num1 = 0;
+// char num2 = 0;
+
+// ✅ 正确：与 datatype（int）保持一致
+int operator = 0;
+int num1 = 0;
+int num2 = 0;
+```
+
+---
+
+将operator改为int后以下代码逻辑还是可以实现功能
+
+```c
+int result(char operator,int num1, int num2)
+{
+	switch (operator)
+	{
+	case '+':
+		return (num2 + num1);
+	case '-':
+		return (num2 - num1);
+	case '*':
+		return (num2 * num1);
+	case '/':
+		return (num2 / num1);
+	default:
+		break;
+	}
+}
+```
+
+![](img1.png)
---
+++ 16_Parsing_infix_expressions/main.c
@@ -0,0 +1,171 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE -1
+
+typedef int datatype;
+typedef struct node {
+	datatype data;
+	struct node* pNext;
+}LinkStack;
+
+//允许传入空的栈顶指针
+LinkStack* Push(LinkStack* top, datatype value)
+{
+	//if (!top)
+	//{
+	//	return NULL;
+	//}
+
+	LinkStack* p;
+	p = (LinkStack*)malloc(sizeof(LinkStack));
+	p->data = value;
+	p->pNext = top;
+	top = p;
+	return top;
+}
+
+LinkStack* Pop(LinkStack* top, datatype* value)
+{
+	if (!top)
+	{
+		return NULL;
+	}
+
+	LinkStack* p;
+	*value = top->data;
+	p = top;
+	top = top->pNext;
+	free(p);
+	return top;
+}
+
+int isEmpety(LinkStack* top)
+{
+	return (top == NULL);
+}
+
+int GetValue(LinkStack* top)
+{
+	if (!top)
+	{
+		return -1;
+	}
+
+	return top->data;
+}
+
+
+int isOperator(char c)
+{
+	switch (c)
+	{
+	case '+':
+	case '-':
+	case '*':
+	case '/':
+		return TRUE;
+	default:
+		return FALSE;
+	}
+}
+
+int Priority(char c)
+{
+	switch (c)
+	{
+	case '+':
+	case '-':
+		return 1;
+	case '*':
+	case '/':
+		return 2;
+	default:
+		return 0;
+	}
+}
+
+int result(char operator,int num1, int num2)
+{
+	switch (operator)
+	{
+	case '+':
+		return (num2 + num1);
+	case '-':
+		return (num2 - num1);
+	case '*':
+		return (num2 * num1);
+	case '/':
+		return (num2 / num1);
+	default:
+		break;
+	}
+}
+
+int main()
+{
+	char expression[] = "8*9-2*5";
+	int index = 0;
+
+	int operator;
+	int num1 = 0;
+	int num2 = 0;
+	int res = 0;
+
+	LinkStack* operatorStack = NULL;
+	LinkStack* numsStack = NULL;
+
+	int priority = 0;
+	int pre_priority = 0;
+	while (expression[index] != '\0')
+	{
+		if (isOperator(expression[index]) == TRUE)
+		{
+			if (!isEmpety(operatorStack))
+			{
+				if (Priority(expression[index]) <= Priority(GetValue(operatorStack)))
+				{
+					numsStack = Pop(numsStack, &num1);
+					numsStack = Pop(numsStack, &num2);
+					operatorStack = Pop(operatorStack, &operator);
+					numsStack = Push(numsStack, result(operator,num1, num2));
+					operatorStack = Push(operatorStack, expression[index]);
+				}
+				else
+				{
+					operatorStack = Push(operatorStack, expression[index]);
+				}
+			}
+			else
+			{
+				operatorStack = Push(operatorStack, expression[index]);
+			}
+		}
+		else 
+		{
+			numsStack = Push(numsStack, expression[index] - '0');
+
+		}
+
+		index++;
+	}
+
+	while (!isEmpety(operatorStack))
+	{
+		numsStack = Pop(numsStack, &num1);
+		numsStack = Pop(numsStack, &num2);
+		operatorStack = Pop(operatorStack, &operator);
+		numsStack = Push(numsStack, result(operator,num1, num2));
+
+	}
+
+	while (!isEmpety(numsStack))
+	{
+		numsStack = Pop(numsStack, &num1);
+		printf("%d\n",num1);
+
+	}
+	printf("end\n");
+	return 0;
+}
